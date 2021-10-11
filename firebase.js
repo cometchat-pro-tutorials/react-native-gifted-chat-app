b5bc55852f1fe1fca1fd632925b645a2f239404f
hieptl <hieptl.developer@gmail.com>
2021-10-11 10:31:56 +0700

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,11 @@ import { fbConfig } from "./env";
 
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { getDatabase, ref, set } from "firebase/database"
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: `${fbConfig.apiKey}`,
-  authDomain: `${fbConfig.authDomain}`,
-  databaseURL: `${fbConfig.databaseURL}`,
+  authDomain: `${fbConfig.authDomain}`, 
   projectId: `${fbConfig.projectId}`,
   storageBucket: `${fbConfig.storageBucket}`,
   messagingSenderId: `${fbConfig.messagingSenderId}`,
@@ -17,6 +15,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const realTimeDb = getDatabase(app);
 
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, realTimeDb, ref, set };
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
